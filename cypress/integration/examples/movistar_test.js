ad557976df6c55c5ab2c39998afea4f5f2a804de
Jose Taulamet <taulajose@gmail.com>
2021-02-21 11:39:52 -0300

    Version 4

--- cypress/integration/examples/movistar_test.js
+++ cypress/integration/examples/movistar_test.js
@@ -31,19 +31,20 @@ describe("tests Movstar", function(){
 
     it('CP003-Validar cuotas en compra de equipo -Cuotas.60 -Equipo.Tercero de la lista -Banco.Credicoop -Tarjeta.Visa', function(){
         //primer paramentro banco, segundo parametro tarjeta, tercer parametro cantidad de cuotas
-      movistarCuotas.filtrarCuotas('Credicoop','Visa','60');
+        movistarCuotas.filtrarCuotas('Credicoop','Visa','60');
      });
 
     it('CP004: Agregar producto al Carrito de compras chequear que se agregue y luego eliminarlo y comprobar si el carrito esta vacio', function(){
+        //addToCart recibe por parametro el equipo que queremos agregar
         movistarCart.addToCart('A02s');
         movistarCart.delToCart();
         movistarCart.CheckEmptyCart();
     });
 
-    it('CP005: Seleccionar marcas de celulares y chequear que devuelva el listado con todos los equipos de la marca correcta',function(){
+    it('CP005: Seleccionar por marcas de celulares y chequear que devuelva el listado con todos los equipos de la marca correcta',function(){
     //por parametro se le puede pasar LG, Motorola, Samsung, Nokia, Alcatel, Apple y entra en cada categoria y recorre todos los equipos de la lista
     //para chequear que todos correspondan con dicha marca y que no se haya filtrado alguno de otra marca. 
-    movistarMarcas.filtrarMarcas('Nokia')
+        movistarMarcas.filtrarMarcas('Nokia')
     });
 
 });
